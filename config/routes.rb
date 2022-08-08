7650f68edf2abdc24b9f830be859071ac2852ae2
sainingo <nickmentor421@gmail.com>
2022-08-08 13:40:26 +0300

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :transactions
    resources :categorys
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "categorys#index"
 end
